f6dede03c391e9efaa25a9b68caff0847a2268c7
Sharon Buhlungu <140716601+sharonb123@users.noreply.github.com>
2024-05-22 20:10:54 +0200

    Add files via upload

---
+++ Clock_Calender/exe_1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+window = Tk()
+window.geometry("400x400")
+window.title("Calender")
+icon = PhotoImage(file='logo1.png')
+window.iconphoto(True,icon)
+window.config(background='black')
+
+
+
+
+
+window.mainloop() #place window on screen/ listens for events
